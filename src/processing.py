a0e4cf52ed8a5544f64a9a25c0aafb0dbe423dfa
admin <magomedrasulisaev296@gmail.com>
2025-10-14 16:36:47 +0300

    Bug Fixes

--- src/processing.py
+++ src/processing.py
@@ -1,16 +1,25 @@
-def filter_by_state(dicts_, state='EXECUTED'):
+from typing import Dict, List
+
+
+def filter_by_state(transactions: List[Dict], state: str='EXECUTED') -> List[Dict]:
+    """
+     фильтрует список операций по статусу.
+
+    Args:
+        transactions: список словарей с операциями
+        state: статус операции по которуму будет фильтроваться список транзакций
+
+    Returns:
+        list: отфильтрованный список транзакций
+    """
     filtered_dicts = []
-    for i in range(len(dicts_)):
-        if dicts_[i]['state'] == state:
-            filtered_dicts += [dicts_[i]]
+    for i in range(len(transactions)):
+        if transactions[i]['state'] == state:
+            filtered_dicts += [transactions[i]]
     return filtered_dicts
-print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                       {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                       {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                       {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], state='EXECUTED'))
 
 
-def sort_by_date(dicts_, reverse=True):
+def sort_by_date(transactions: List[Dict], reverse: bool =True) -> List[Dict]:
     """
     Сортирует список операций по дате.
 
@@ -21,18 +30,4 @@ def sort_by_date(dicts_, reverse=True):
     Returns:
         list: отсортированный список операций
     """
-    return sorted(dicts_, key=lambda x: x['date'], reverse=reverse)
-
-print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], reverse=True))
-
-
-
-
-
-
-
-
-
-
-    #if sort_type == 'Decrease':
-     #   for i in range(len(dicts_)):
+    return sorted(transactions, key=lambda x: x['date'], reverse=reverse)
